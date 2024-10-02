611f2619e45f278821ac76591ab00450f814bacd
Brian Bollen <briancbollen@gmail.com>
2024-10-02 08:39:36 -0600

    Finished configuring component library

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,6 +2,12 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import postcss from "rollup-plugin-postcss";
+
+
+
+import { terser } from "@rollup/plugin-terser";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 export default [
     {
@@ -22,11 +28,15 @@ export default [
             resolve(),
             commonjs(),
             typescript({ tsconfig: "./tsconfig.json" }),
+            postcss(),
+            terser()
         ],
     },
     {
         input: "dist/esm/types/index.d.ts",
         output: [{ file: "dist/index.d.ts", format: "esm" }],
         plugins: [dts()],
+        external: [/\.scss$/, /\.css$/],
+
     },
 ];
